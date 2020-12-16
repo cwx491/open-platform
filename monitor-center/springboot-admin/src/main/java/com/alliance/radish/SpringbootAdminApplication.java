cc93d9e2381eb10970939bee5acaf9c251bfcc47
cwx491 <604298674@qq.com>
2020-12-16 15:47:40 +0800

    优化eureka-server

--- monitor-center/springboot-admin/src/main/java/com/alliance/radish/SpringbootAdminApplication.java
+++ monitor-center/springboot-admin/src/main/java/com/alliance/radish/SpringbootAdminApplication.java
@@ -2,14 +2,12 @@ package com.alliance.radish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class SpringbootAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(SpringbootAdminApplication.class, args);
     }
 
 }
